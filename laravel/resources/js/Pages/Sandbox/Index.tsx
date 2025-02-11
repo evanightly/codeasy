b2f2392c23e2846384b9d5bc1371927bcc8094cd
Galur Arasy Lumintang <adamhenderson3x3@gmail.com>
2025-02-11 23:28:02 +0700

    update: changed monaco to codemirror

--- laravel/resources/js/Pages/Sandbox/Index.tsx
+++ laravel/resources/js/Pages/Sandbox/Index.tsx
@@ -1,5 +1,6 @@
-import Editor from '@monaco-editor/react';
-import { useState } from 'react';
+import { python } from '@codemirror/lang-python';
+import CodeMirror from '@uiw/react-codemirror';
+import { useCallback, useState } from 'react';
 
 export default function Index() {
     const [code, setCode] = useState("print('Ngide')");
@@ -15,15 +16,20 @@ export default function Index() {
 
         setOutput(res.data);
     };
+
+    const onChange = useCallback((val, viewUpdate) => {
+        console.log('val:', val);
+        setCode(val);
+    }, []);
+
     return (
         <div>
             <h1>Sandbox</h1>
-
-            <Editor
-                height="90vh"
-                defaultLanguage="python"
+            <CodeMirror
                 value={code}
-                onChange={(value, event) => setCode(value || '')}
+                height="200px"
+                extensions={[python()]}
+                onChange={onChange}
             />
 
             <button onClick={handleSubmit} type="submit" className="bg-red-500">
@@ -33,11 +39,18 @@ export default function Index() {
             <div className="flex flex-col gap-2">
                 {output.map((out, i) => {
                     if (out.type === 'image') {
-                        return <img key={i} src={out.content} alt="output" />;
+                        return (
+                            <img
+                                className="mx-auto w-[50rem] rounded bg-gray-100"
+                                key={i}
+                                src={out.content}
+                                alt="output"
+                            />
+                        );
                     }
 
                     return (
-                        <div key={i} className="p-3">
+                        <div key={i} className="rounded bg-gray-100 p-3">
                             {out.content}
                         </div>
                     );
