520bcba1aa46f2216c2003e971a776fed5fc0243
Galur Arasy Lumintang <adamhenderson3x3@gmail.com>
2025-02-16 00:39:46 +0700

    feat: integrate SonnerToaster for improved error handling and add axios error interceptor

--- laravel/resources/js/app.tsx
+++ laravel/resources/js/app.tsx
@@ -1,6 +1,6 @@
 import '../css/app.css';
 import './bootstrap';
-
+import { Toaster as SonnerToaster } from '@/Components/UI/sonner';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
@@ -17,7 +17,22 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <>
+                <SonnerToaster
+                    toastOptions={
+                        {
+                            // https://github.com/shadcn-ui/ui/issues/2234
+                        }
+                    }
+                    theme="light"
+                    richColors
+                    duration={2000}
+                    closeButton
+                />
+                <App {...props} />
+            </>,
+        );
     },
     progress: {
         color: '#4B5563',
