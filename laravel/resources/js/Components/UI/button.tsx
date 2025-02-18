a093fceaf47d48435aa7aae3719302adaf050310
Galur Arasy Lumintang <adamhenderson3x3@gmail.com>
2025-02-18 10:36:34 +0700

    feat: add ripple effect to button component and enhance button variants with additional styles

--- laravel/resources/js/Components/UI/button.tsx
+++ laravel/resources/js/Components/UI/button.tsx
@@ -3,6 +3,7 @@ import { type VariantProps, cva } from 'class-variance-authority';
 import * as React from 'react';
 
 import { ny } from '@/Lib/Utils';
+import { LoaderCircle } from 'lucide-react';
 
 const buttonVariants = cva(
     'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
@@ -15,6 +16,14 @@ const buttonVariants = cva(
                 outline:
                     'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
                 secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
+                tertiary: 'bg-tertiary text-tertiary-foreground shadow-sm hover:bg-tertiary/80',
+                quaternary:
+                    'bg-quaternary text-quaternary-foreground shadow-sm hover:bg-quaternary/80',
+                success: 'bg-success text-success-foreground shadow-sm hover:bg-success/90',
+                warning: 'bg-warning text-warning-foreground shadow-sm hover:bg-warning/90',
+                info: 'bg-info text-info-foreground shadow-sm hover:bg-info/90',
+                create: 'bg-create text-create-foreground shadow-sm hover:bg-create/90',
+                update: 'bg-update text-update-foreground shadow-sm hover:bg-update/90',
                 ghost: 'hover:bg-accent hover:text-accent-foreground',
                 link: 'text-primary underline-offset-4 hover:underline',
             },
@@ -36,17 +45,36 @@ export interface ButtonProps
     extends React.ButtonHTMLAttributes<HTMLButtonElement>,
         VariantProps<typeof buttonVariants> {
     asChild?: boolean;
+    disableRipple?: boolean;
+    loading?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-    ({ className, variant, size, asChild = false, ...props }, ref) => {
+    (
+        {
+            className,
+            variant,
+            size,
+            asChild = false,
+            disableRipple = false,
+            loading = false,
+            ...props
+        },
+        ref,
+    ) => {
         const Comp = asChild ? Slot : 'button';
         return (
             <Comp
-                className={ny(buttonVariants({ variant, size, className }))}
                 ref={ref}
-                {...props}
-            />
+                className={ny(
+                    buttonVariants({ variant, size, className }),
+                    !disableRipple && 'ripple', // Include the ripple class only if disableRipple is false
+                )}
+                {...props}>
+                {loading && <LoaderCircle className="animate-spin" />}
+
+                {props.children}
+            </Comp>
         );
     },
 );
