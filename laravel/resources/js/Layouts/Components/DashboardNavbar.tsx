7a4d81b3e910911092cddb6b5cea929066a705ae
Galur Arasy Lumintang <adamhenderson3x3@gmail.com>
2025-02-28 04:19:35 +0700

    update: add new themes and handle SidebarTrigger icon colors for multiple themes

--- laravel/resources/js/Layouts/Components/DashboardNavbar.tsx
+++ laravel/resources/js/Layouts/Components/DashboardNavbar.tsx
@@ -17,7 +17,7 @@ const DashboardNavbar = ({
     return (
         <header className='flex h-16 shrink-0 items-center gap-2 border-b px-4 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12'>
             <div className='flex items-center gap-2'>
-                <SidebarTrigger title='Toggle Sidebar' className='-ml-1' />
+                <SidebarTrigger title='Toggle Sidebar' className='-ml-1 text-foreground' />
                 <Separator orientation='vertical' className='mr-2 h-4' />
                 {breadcrumbs && <GenericBreadcrumb breadcrumbs={breadcrumbs} />}
                 {navbarStart}
