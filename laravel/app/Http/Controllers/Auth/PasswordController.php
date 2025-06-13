3e5bbc70618f708b462571718dcf18597a2ba83e
evanightly <adamhenderson3x3@gmail.com>
2025-06-14 04:10:37 +0700

    fix: 422 error when updating password

--- laravel/app/Http/Controllers/Auth/PasswordController.php
+++ laravel/app/Http/Controllers/Auth/PasswordController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
@@ -12,7 +11,7 @@ class PasswordController extends Controller {
     /**
      * Update the user's password.
      */
-    public function update(Request $request): RedirectResponse {
+    public function update(Request $request) {
         $validated = $request->validate([
             'current_password' => ['required', 'current_password'],
             'password' => ['required', Password::defaults(), 'confirmed'],
@@ -22,6 +21,6 @@ class PasswordController extends Controller {
             'password' => Hash::make($validated['password']),
         ]);
 
-        return back();
+        // return back();
     }
 }
